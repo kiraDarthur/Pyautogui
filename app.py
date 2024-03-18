4c934f928b6af52660bced048aedaa22ef10b106
kiraDarthur <arthurandrade8713@gmail.com>
2024-03-18 13:12:39 +0000

    inicio de projeto

---
+++ app.py
@@ -0,0 +1,10 @@
+#logica do programa
+
+# clicar e digitar meu usuario
+# clicar e digitar  , minha senha
+# clicar em enter
+ # extrair cada produto
+ # clicar e digitar produto
+ # clicar e digitar quantidade
+ # clicar e digitar preço
+ # 0clicar em registrar
